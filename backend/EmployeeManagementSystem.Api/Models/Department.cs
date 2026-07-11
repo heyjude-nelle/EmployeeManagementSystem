f2dcbb73c64508baa05122413b950b266ec2eb47
Jude Torres <judinelle.torres@dxc.com>
2026-07-11 19:31:12 +0800

    feat: add department enum

---
+++ backend/EmployeeManagementSystem.Api/Models/Department.cs
@@ -0,0 +1,14 @@
+namespace EmployeeManagementSystem.Api.Models;
+
+public enum Department
+{
+    Accounting,
+    Engineering,
+    HumanResources,
+    Legal,
+    Management,
+    Marketing,
+    Operations,
+    Sales,
+    Support
+}
