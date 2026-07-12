3bd97ea48106b74276d890e0ad066f4498b2cb2e
Jude Torres <judinelle.torres@dxc.com>
2026-07-13 02:26:05 +0800

    feat: enable swagger ui with swashbuckle

--- backend/EmployeeManagementSystem.Api/Program.cs
+++ backend/EmployeeManagementSystem.Api/Program.cs
@@ -7,8 +7,9 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -26,7 +27,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
